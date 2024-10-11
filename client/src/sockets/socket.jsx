cff4fb96d4414a7be180b8a769355e234bb8e1e9
KKA-0 <karanyobro@gmail.com>
2024-10-12 00:11:25 +0530

    socket server host uri

--- client/src/sockets/socket.jsx
+++ client/src/sockets/socket.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io(`http://localhost:3000`);
+export const socket = io(`${import.meta.env.VITE_APP_DOMAIN}`);
 
 export default function socketcom() {
 
